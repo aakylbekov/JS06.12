64beee241534ee9157d1ee0541913cdb844c679a
Ален Акылбеков <@almaty.shag>
2018-12-06 21:44:43 +0600

    Добавьте файлы проекта.

---
+++ JS/Scripts/CW/JavaScript.js
@@ -0,0 +1,17 @@
+﻿
+
+$('#button').on('click', function ()
+{
+    $('#ul1').append(
+        $('<li>').append('#field').val()).append(
+        $('<button>').html('delete').addClass('btn-danger').on('click', function () {
+            $(this).parents('li').remove();
+             
+        })
+   )
+})
+
+//var el = $('<li>').html().on('click', function () {
+   
+//});
+//el).append('<button>').append($('<p>'))
